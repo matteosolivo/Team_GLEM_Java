e5e67be7ab003a08468c9d429c8f981e96a63c8a
giovannamasotti <giovanna.masotti@edu.unife.it>
2025-11-26 18:28:33 +0100

    Add files via upload

---
+++ progetto-java/src/main/java/it/university/model/Item.java
@@ -0,0 +1,9 @@
+package it.university.model;
+
+abstract class Item {
+    protected Integer id;
+    protected String name;
+
+    public Integer getId() { return id; }
+    public String getName() { return name; }
+}

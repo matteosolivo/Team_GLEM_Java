ab0a01be1883ebba57b8908c4cbb4c1dbaec399f
evaccarino <elisabetta.vaccarino@edu.unife.it>
2025-11-29 10:08:49 +0100

    Singleton su GradeRepository

--- progetto-java/src/main/java/it/university/repository/GradeRepository.java
+++ progetto-java/src/main/java/it/university/repository/GradeRepository.java
@@ -6,8 +6,22 @@ import java.util.List;
 import it.university.model.Grade;
 
 public class GradeRepository implements ICollectionRepository<Grade>{
-    private final List<Grade> grades = new ArrayList<>();
+    
+	private static GradeRepository istance;
+	
+	private final List<Grade> grades = new ArrayList<>();
 
+    private GradeRepository() {
+    	System.out.println("Grade Repository creato");
+    }
+    
+    public static GradeRepository getIstance() {
+    	if(istance == null) {
+    		istance = new GradeRepository();
+    	}
+    	return istance;
+    }
+    
     @Override
     public void save(Grade grade) {
         grades.add(grade);

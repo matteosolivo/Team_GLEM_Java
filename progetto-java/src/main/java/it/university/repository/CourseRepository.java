ffec773f4f6f604d0d7b0bffd53b8417736617e6
evaccarino <elisabetta.vaccarino@edu.unife.it>
2025-11-29 10:07:42 +0100

    Singleton su CourseRepository

--- progetto-java/src/main/java/it/university/repository/CourseRepository.java
+++ progetto-java/src/main/java/it/university/repository/CourseRepository.java
@@ -4,8 +4,22 @@ import it.university.model.Course;
 
 public class CourseRepository extends AbstractRepository<Course, Integer> {
 
+	private static CourseRepository istance;
+	
+	private CourseRepository() {
+		System.out.println("Course Repository creato");
+	}
+	
+	public static CourseRepository getIstance() {
+		if(istance == null) {
+			istance = new CourseRepository();
+		}
+		return istance;
+	}
+	
     @Override
     protected Integer extractId(Course course) {
         return course.getId();
     }
 }
+

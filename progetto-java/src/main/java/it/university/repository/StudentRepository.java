8628821fca17ece40f575ae8c27b33680d0f435a
evaccarino <elisabetta.vaccarino@edu.unife.it>
2025-11-29 10:09:41 +0100

    Singleton su StudentRepository
    Implemented singleton pattern for StudentRepository.

--- progetto-java/src/main/java/it/university/repository/StudentRepository.java
+++ progetto-java/src/main/java/it/university/repository/StudentRepository.java
@@ -4,6 +4,19 @@ import it.university.model.Student;
 
 public class StudentRepository extends AbstractRepository<Student, Integer> {
 
+	private static StudentRepository istance;
+	
+	private StudentRepository() {
+		System.out.println("Student Repository creato");
+	}
+	
+	public static StudentRepository getIstance() {
+		if(istance == null) {
+			istance = new StudentRepository();
+		}
+		return istance;
+	}
+	
     @Override
     protected Integer extractId(Student student) {
         return student.getId();

94efb71173f3f389c2555d9646fd85039abc0af5
giovannamasotti <giovanna.masotti@edu.unife.it>
2025-12-02 11:22:59 +0100

    commenti GradeRepository.java

--- progetto-java_V0_commented/src/main/java/it/university/repository/GradeRepository.java
+++ progetto-java_V0_commented/src/main/java/it/university/repository/GradeRepository.java
@@ -4,8 +4,9 @@ import it.university.model.Grade;
 import java.util.*;
 
 public class GradeRepository {
+    //CREA UNA LIST DI OGGETTI DI TIPO GRADE
     private List<Grade> grades = new ArrayList<>();
 
-    public void save(Grade g) { grades.add(g); }
-    public List<Grade> findAll() { return grades; }
+    public void save(Grade g) { grades.add(g); } //SALVA UN OGGETTO GRADE AGGIUNGENDOLO ALLA LIST
+    public List<Grade> findAll() { return grades; } //RITORNA LA LIST DI OGGETTI GRADE
 }

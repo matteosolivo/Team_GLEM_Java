0dec0e2159aa23a6a8c0a12c723bab53133f5713
evaccarino <elisabetta.vaccarino@edu.unife.it>
2025-12-02 12:43:46 +0100

    Commenti su EnrollmentService nel progetto_v0

--- progetto-java_V0_commented/src/main/java/it/university/service/EnrollmentService.java
+++ progetto-java_V0_commented/src/main/java/it/university/service/EnrollmentService.java
@@ -4,15 +4,22 @@ import it.university.model.Enrollment;
 import it.university.repository.EnrollmentRepository;
 import java.util.List;
 
+//L'enrollmentservice gestisce un oggetto di tipo EnrollmentRepository
 public class EnrollmentService {
+    
     private EnrollmentRepository repo = new EnrollmentRepository();
 
+    //Questo metodo invoca save() di EnrollmentRepository, che a sua volta chiama add() di List, in modo da inserire un oggetto Enrollment nel repository 
     public void enrollStudent(Enrollment e) { repo.save(e); }
+
+    //Questo metodo utilizza findAll() di EnrollmentRepository per restituire la lista intera, solo se la lista non è vuota
     public List<Enrollment> list() { 
         if (repo.findAll().isEmpty()){
             System.out.println("Nessuna iscrizione trovata");
         }
         return repo.findAll(); 
     }
+
+    //Invoca exists() di EnrollmentRepository per verificare la presenza di un'iscrizione nel repository
     public boolean isEnrolled(int studentId, int courseId) { return repo.exists(studentId, courseId); }
 }
